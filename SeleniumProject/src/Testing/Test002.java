b29931ea1b54565defc00000db5161d438c2c414
Radhika Santapuri <radhika.santapuri@gmail.com>
2018-11-17 19:06:00 -0600

    more projects

---
+++ SeleniumProject/src/Testing/Test002.java
@@ -0,0 +1,29 @@
+package Testing;
+
+public class Test002 {
+	Test001 test1 = new Test001();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+	
+	public int sum(int a, int b) {
+		
+		int sumResult = a + b;
+		
+		return sumResult;
+	}
+	
+	public int sub(int a, int b) {
+		
+		int subResult = a - b;
+		
+		return subResult;
+		
+	}
+	
+	public void mul(int a, int b) {
+		
+	}
+
+}
